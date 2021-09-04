3f6e4606bb1417e1f8d27b78e3cb2117085df8b3
U.A. Yanık <ates.yanik@hva.nl>
2021-09-04 18:00:19 +0200

    Filtered Navigation added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
-    <h1>Project Planner</h1>
+    <!-- $event is used to be able to use the data passed with $emit -->
+    <FilterNav @filterChange="current = $event" :current="current"/> 
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -15,15 +16,17 @@
 <script>
 // @ is an alias to /src
 import SingleProject from "../components/SingleProject.vue"
+import FilterNav from "../components/FilterNav.vue"
 
 export default {
   name: "Home",
   data() {
     return {
       projects: [],
+      current: 'all'
     };
   },
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   mounted() {
     fetch("http://localhost:3000/projects")
       .then((res) => res.json())
@@ -41,6 +44,17 @@ export default {
         return project.id === id
       })
       p.complete = !p.complete
+    }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === "completed") {
+        return this.projects.filter( project => project.complete )
+      } 
+      if (this.current === "ongoing"){
+        return this.projects.filter( project => !project.complete)
+      }
+      return this.projects;
     }
   }
 };
